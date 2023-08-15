aad6d31b71f34734cead516a748ee9a4dfc0c700
TF-64 <asd@asd.asd>
2023-08-15 21:32:45 +0300

    auto building and linkage

---
+++ auto_compiler.py
@@ -0,0 +1,32 @@
+from sys import argv
+from os import system, listdir
+from os.path import isfile, join
+from pathlib import Path
+
+
+path = str(Path().resolve())
+
+def compile_all(compile_path, build_path):
+    
+    for file in listdir(compile_path):
+        if isfile(join(compile_path, file)):
+           if (file[-2] + file[-1]) == '.c' and file != '.DS_Store':
+               system(f'i686-elf-gcc -c {join(compile_path, file)} -o {build_path}/{file[0:len(file)-2]}.o -Isrc/include -lgcc -ffreestanding -O2 -nostdlib')
+        else:
+            compile_all(compile_path + '/' + file, build_path)
+
+
+# compile path, build path, name of .bin file
+
+compile_all(path + "/src/include", path + "/build")
+
+
+#system('i686-elf-gcc -T src/kernel/arch/linker.ld -o build/Entropy.bin  -ffreestanding -O2 -nostdlib build/
+#    boot.o build/kernel.o build/abort.o build/memcmp.o build/memcpy.o build/memmove.o build/memset.o build/printf.o build/putch
+#   ar.o build/strlen.o build/tty.o -lgcc')
+
+
+
+
+
+
