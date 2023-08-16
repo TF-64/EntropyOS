e1e995fb7e31e74d6ef9eb1eb5fa71884e0abf47
TF-64 <asd@asd.asd>
2023-08-16 21:04:39 +0300

    libjemalloc error linking error fixed

--- auto_linker.py
+++ auto_linker.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 path = str(Path().resolve())
 
-base_command = f"i686-elf-gcc -T {path}/src/kernel/arch/linker.ld -o {path}/build/{argv[1]}.bin -ffreestanding -O2 -nostdlib `jemalloc-config --libdir`/libjemalloc.a -lstdc++ -lpthread "
+base_command = f"i686-elf-gcc -T {path}/src/kernel/arch/linker.ld -o {path}/build/{argv[1]}.bin -ffreestanding -O2 -nostdlib "
 
 
 for file in listdir(f"{path}/build"):
